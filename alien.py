f8d6a78668a84463a9f35e0da4825ce7a5b04502
JINSEH <e.p.gamerstv@gmail.com>
2025-01-13 15:02:45 +0800

    Removed bullet logic for aliens, updated readme

--- alien.py
+++ alien.py
@@ -1,6 +1,5 @@
 import pygame
 from pygame.sprite import Sprite
-from bullet import Bullet
 
 class Alien(Sprite):
     def __init__(self, ai_game):
@@ -22,31 +21,10 @@ class Alien(Sprite):
         # Store the alien's exact horizontal position
         self.x = float(self.rect.x)
 
-        self.shoot_timer = 0
-        self.shoot_interval = 200
-
-        self.initial_first_row_y = ai_game.first_row_y  # Add this line
-
     def update(self):
         """Move the alien to the right or left"""
         self.x += self.settings.alien_speed * self.settings.fleet_direction
         self.rect.x = self.x
-        self.shoot_timer += 1
-
-        if self.shoot_timer >= self.shoot_interval and self._is_in_first_row():
-            self.shoot_bullet()
-            self.shoot_timer = 0
-
-    def shoot_bullet(self):
-        """Create a bullet fired by the alien"""
-        new_bullet = Bullet(self.ai_game, source="alien")
-        new_bullet.rect.midbottom = self.rect.midbottom
-        self.ai_game.bullets.add(new_bullet)
-
-    def _is_in_first_row(self):
-        """Check if the alien is in the first row"""
-        margin = self.rect.height // 2  # Allow half the height of the alien as margin
-        return abs(self.rect.y - self.initial_first_row_y) <= margin
 
     def check_edges(self):
         """Return true if alien is at the edge of the screen"""
